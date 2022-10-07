f6b551c106deb22f98e786d05c123705236f2748
Allison Andrea Rodriguez Sanchez <allisonandrea712@gmail.com>
2022-10-07 11:07:59 -0500

    Examen T2

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
     env: {
-        baseURL: ('')
+        baseURL: ('https://localhost:44320/')
     },
   e2e: {
     setupNodeEvents(on, config) {
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,26 +1,26 @@
 describe('Examen T2 CALIDAD', () => {
-    beforeEach('Iniciar sesion para entrar a mi Biblioteca', () => {
+    beforeEach('Probar iniciar sesion', () => {
         cy.visit(Cypress.env('baseUrl'))
-        cy.get('#agregarBiblioteca').click()
-        cy.get('#usuario').type('admin')
-        cy.get('#contrasena').type('admin')
-        cy.get('#iniciarSesion').click()
+        cy.get('#add_Biblioteca').click()
+        cy.get('#user_Name').type('admin')
+        cy.get('#password').type('admin')
+        cy.get('#iniciar_Sesion').click()
 
     })
     it('Agregar Libro y entrar a Biblioteca', () => {
         cy.visit(Cypress.env('baseUrl'))
-        cy.get('#agregarBiblioteca').click()
-        cy.get('#miBiblioteca').click()
+        cy.get('#add_Biblioteca').click()
+        cy.get('#mi_Biblioteca').click()
         cy.visit(Cypress.env('pageUrl') + '/Biblioteca')
         cy.location('pathname').should('eq', '/Biblioteca')
 
     })
     it('Cambiar estado: leyendo en la Biblioteca', () => {
         cy.visit(Cypress.env('baseUrl') + '/Biblioteca')
-        cy.get('#estadolibroLeyendo').click()
+        cy.get('#estadoLeyendo').click()
     })
     it('Cambiar estado: terminado en la Biblioteca', () => {
         cy.visit(Cypress.env('baseUrl') + '/Biblioteca')
-        cy.get('#estadolibroTerminado').click()
+        cy.get('#estadoTerminado').click()
     })
 })
